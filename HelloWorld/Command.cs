d49fd1571cda2510dfe00f0deccb0ec7929cb782
Whitney Schmidt <whschm@microsoft.com>
2021-01-06 15:51:14 -0500

    add //hellonewmain

--- HelloWorld/Command.cs
+++ HelloWorld/Command.cs
@@ -6,7 +6,7 @@ using System.Globalization;
 using System.Threading;
 using System.Threading.Tasks;
 using Task = System.Threading.Tasks.Task;
-
+//hellonewmain
 namespace HelloWorld
 {
     /// <summary>
