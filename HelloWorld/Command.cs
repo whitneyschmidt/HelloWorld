739d7c58dac666ffb3f68a2be8bd86f9519b8b74
Whitney Schmidt <whschm@microsoft.com>
2021-01-07 15:49:53 -0500

    change to //hello123

--- HelloWorld/Command.cs
+++ HelloWorld/Command.cs
@@ -6,7 +6,7 @@ using System.Globalization;
 using System.Threading;
 using System.Threading.Tasks;
 using Task = System.Threading.Tasks.Task;
-//hellonewmain
+//hello123
 namespace HelloWorld
 {
     /// <summary>
