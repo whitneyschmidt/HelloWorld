f5028f731f37078ce03f66ad15633257f41aedc9
Whitney Schmidt <whschm@microsoft.com>
2021-02-22 17:28:57 -0500

    commit 1

--- HelloWorld/Command.cs
+++ HelloWorld/Command.cs
@@ -7,6 +7,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using Task = System.Threading.Tasks.Task;
 //hellonewmain
+//update1
+//up[date 2]
+//blah blah
 namespace HelloWorld
 {
     /// <summary>
