5f75ad06cc2ea856085b8a979854b84709fc9d76
Whitney Schmidt <whschm@microsoft.com>
2021-03-30 13:00:27 -0400

    this is a comment

--- HelloWorld/Command.cs
+++ HelloWorld/Command.cs
@@ -6,7 +6,7 @@ using System.Globalization;
 using System.Threading;
 using System.Threading.Tasks;
 using Task = System.Threading.Tasks.Task;
-// conflicting comment comment comment
+// this is a comment
 namespace HelloWorld
 {
     /// <summary>
