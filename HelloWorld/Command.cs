4afe5e075c301777c5c2fa1118022d97124494dc
Whitney Schmidt <whschm@microsoft.com>
2021-09-08 18:52:31 -0400

    this is another comment

--- HelloWorld/Command.cs
+++ HelloWorld/Command.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Task = System.Threading.Tasks.Task;
 // this is a comment
+// this is another comment
 namespace HelloWorld
 {
     /// <summary>
